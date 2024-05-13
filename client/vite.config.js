46943e0a5181631dd090ada8a0253081d4c02b83
_prince_30 <116263012+PrinceMishra30@users.noreply.github.com>
2024-05-13 05:33:46 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: "./", // Specify the root directory if it's not the current directory
+  build: {
+    outDir: "dist", // Specify the output directory for the production build
+    rollupOptions: {
+      input: "./src/main.jsx", // Specify the entry point for your React application
+    },
+  },
+});
